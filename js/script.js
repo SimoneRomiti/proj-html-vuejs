5b7c361093ab3222541d28a431fb9215eb34a91d
Simone Romiti <sr1992ei@gmail.com>
2021-01-12 12:48:43 +0100

    Header con nav dinamica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-
+var app = new Vue(
+  {
+    el: '#container',
+    data: {
+      headerNav: [
+        {
+          name: 'HOME',
+          link: '#'
+        },
+        {
+          name: 'ABOUT US',
+          link: '#'
+        },
+        {
+          name: 'FACILITIES',
+          link: '#'
+        },
+        {
+          name: 'MEMBERSHIP',
+          link: '#'
+        },
+        {
+          name: 'TESTIMONIALS',
+          link: '#'
+        },
+        {
+          name: 'BLOG',
+          link: '#'
+        }
+      ]
+    }
+  }
+)
